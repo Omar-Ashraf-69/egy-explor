ef4a824493259d466e77f9a05bc881d30f7e8904
OmarAsharf62 <omarbondok62@gmail.com>
2024-04-20 02:26:56 +0200

    Update appbar widget with user profile picture and username
    Import necessary packages and update the appbar widget to display the user's
    profile picture and username dynamically. Use BlocProvider to access user data
    and show either the profile picture from network or a default image. Adjust
    padding and alignment for better UI presentation.

--- lib/features/explore/presentation/views/widgets/appbar.dart
+++ lib/features/explore/presentation/views/widgets/appbar.dart
@@ -1,10 +1,14 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:egy_exlpor/core/utils/assets.dart';
 import 'package:egy_exlpor/core/utils/colors.dart';
 import 'package:egy_exlpor/core/utils/custom_icon_button.dart';
 import 'package:egy_exlpor/core/utils/styles.dart';
 import 'package:egy_exlpor/features/notification/presentation/view/notification_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ionicons/ionicons.dart';
+
+import '../../../../../core/managers/get_user_cubit/user_details_cubit.dart';
 
 PreferredSizeWidget exploreViewAppBar(BuildContext context) {
   return AppBar(
@@ -25,14 +29,27 @@ PreferredSizeWidget exploreViewAppBar(BuildContext context) {
               SizedBox(
                 height: 40,
                 width: 40,
-                child: Image.asset(
-                  AssetsData.profilePic,
-                ),
+                child: BlocProvider.of<UserCubit>(context)
+                        .user!
+                        .profilePic
+                        .isNotEmpty
+                    ? CircleAvatar(
+                        radius: 40,
+                        backgroundColor: Colors.grey,
+                        onBackgroundImageError: (exception, stackTrace) =>
+                            const Icon(Icons.error),
+                        backgroundImage: CachedNetworkImageProvider(
+                          BlocProvider.of<UserCubit>(context).user!.profilePic,
+                        ),
+                      )
+                    : Image.asset(
+                        AssetsData.profilePic,
+                      ),
               ),
               Padding(
-                padding: const EdgeInsets.only(left: 4.0, right: 8),
+                padding: const EdgeInsets.only(left: 8.0, right: 10),
                 child: Text(
-                  "Omar Ashraf",
+                  BlocProvider.of<UserCubit>(context).user!.userName,
                   style: Styles.textStyle16.copyWith(
                     fontWeight: FontWeight.w600,
                   ),
